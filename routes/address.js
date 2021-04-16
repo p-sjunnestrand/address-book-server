6f8d39b7706908fd187c23f4ee6f6a3ac432893a
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-04-16 23:00:20 +0200

    added address router and json with one address + try fs command

--- routes/address.js
+++ routes/address.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const fs = require('fs');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('address book');
+  fs.readFile("addresses.json", (err, data) => {
+    if (err){
+      console.log(err);
+    }
+
+    const addresses = JSON.parse(data);
+    res.send(addresses)
+  })
 });
 
 module.exports = router;
